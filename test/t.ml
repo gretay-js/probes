06099c0c4acec20aede9b4dffb54ad4cae4cd4cd
Greta Yorsh <gyorsh@janestreet.com>
2020-01-17 16:24:19 +0000

    test for osdt probes

---
+++ test/t.ml
@@ -0,0 +1,47 @@
+(* handlers *)
+let h0 name =
+  Printf.printf "h0:%s\n" name
+
+let h1 name n fl ls o =
+  Printf.printf "h0:%s:%d:%f:%s:%s\n" name n fl
+    (String.concat "" (List.map Int.to_string ls))
+    (Option.value (Option.map (Printf.sprintf "Some %d") o) ~default:"None")
+
+let h2 name x str =
+  Printf.printf "h0:%s:%d:%s\n" name x str
+
+let h3 name x =
+  Printf.printf "h0:%s:0x%Lx\n" name x
+
+let h4 name x y =
+  Printf.printf "h0:%s:%d:%d\n" name x y
+
+(* code *)
+let test1 x y =
+  [%probe "test1" h4 x y];
+  let z =
+    if x > y  then
+      ([%probe "test_arg" h2 (x+y) "true_branch"];
+       x - y)
+    else
+      ([%probe "test_arg" h2 x "false_branch"];
+       y - x)
+  in
+  assert (not (y = 0));
+  let fl = Float.((float_of_int x) /. (float_of_int y)) in
+  [%probe "test_manyarg" h1 x fl [x;y;z] (Some z)];
+  z
+[@@inline never]
+
+let () =
+  [%probe "main" h3 0x45L];
+  let r =
+    if ((Array.length Sys.argv) = 3) then
+      ([%probe "test_noarg" h0];
+        test1 (int_of_string Sys.argv.(1)) (int_of_string Sys.argv.(2)))
+    else Int.max_int
+  in
+    Printf.printf "res=%d\n" r;
+  ()
+
+
