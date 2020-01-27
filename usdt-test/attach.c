d7b6d1199cc8d75f1fc399da191ab27361407e74
Greta Yorsh <gyorsh@janestreet.com>
2020-01-27 15:27:46 +0000

    arg to attach

--- usdt-test/attach.c
+++ usdt-test/attach.c
@@ -9,14 +9,23 @@
 
 
 int main(int argc, char *argv[]) {
-  size_t addr = 0x400078;
+  unsigned long addr;
   pid_t cpid = -1; // pid to attach ptrace to
-  if(argc!=2) {
-    fprintf(stderr, "Missing arguments\n");
+  if(argc!=3) {
+    fprintf(stderr,
+            "Missing arguments.\n\
+             Usage: attach %s <pid> <hex-addr-to-rewrite>\n",
+            argv[0]
+            );
     return 1;
   }
   if (sscanf(argv[1], "%d", &cpid) != 1) {
     fprintf(stderr, "Cannot read pid %s\n", argv[1]);
+    return 1;
+  };
+
+  if (sscanf(argv[2], "%lx", &addr) != 1) {
+    fprintf(stderr, "Cannot read hexadecimal address to rewrite %s\n", argv[1]);
     return 1;
   };
 
@@ -32,10 +41,15 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  int data = ptrace(PTRACE_PEEKTEXT, cpid, addr, NULL);
-  fprintf (stderr, "%x\n", data);
+  fprintf(stderr, "Modifying address %lx in running process %d\n", addr, cpid);
+  unsigned int data = ptrace(PTRACE_PEEKTEXT, cpid, addr, NULL);
+  fprintf (stderr, "before: %x\n", data);
+  if ((data & 0xff) != 0x3d) {
+    fprintf(stderr, "Unexpected instruction! %x\n", (data & 0xff));
+    return 1;
+  }
   data = (data & ~0xff) | 0xe8;
-  fprintf (stderr, "%x\n", data);
+  fprintf (stderr, "after:  %x\n", data);
   ptrace(PTRACE_POKETEXT, cpid, addr, data);
 
   if(ptrace(PTRACE_CONT, cpid, NULL, NULL)==-1) {
@@ -45,13 +59,14 @@ int main(int argc, char *argv[]) {
 
   wait(&status);
   if(!WIFEXITED(status)) {
-    fprintf(stderr, "child did not exit\n");
-
     int signum = WSTOPSIG(status);
     struct user_regs_struct regs;
     ptrace(PTRACE_GETREGS, cpid, NULL, &regs);
     printf ("signal: %d, eip: 0x%08llx\n", signum, regs.rip);
     return status;
+  }
+  else {
+    fprintf(stderr, "child exited\n");
   }
   return 0;
 }
