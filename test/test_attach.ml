c0e24453373043be824868fedb8e2755943ff494
Greta Yorsh <gyorsh@janestreet.com>
2020-06-18 16:23:11 +0100

    non-terminating fixed

--- test/test_attach.ml
+++ test/test_attach.ml
@@ -4,11 +4,11 @@ let test prog =
   let bpf = false in
   let pid = P.trace_test_lib ~prog ~args:[] ~bpf in
   Unix.sleep 1;
-  P.attach_test_lib ~pid ~bpf ~actions:(P.Selected (P.Enable "fooia"));
+  P.attach_test_lib ~pid ~bpf ~enable:false;
   Unix.sleep 1;
   P.attach_test_lib ~pid ~bpf ~enable:true;
   Unix.kill pid Sys.sigkill;
-  P.wait pid
+  P.wait pid ~prog
 
 let () =
   test Sys.argv.(1)
