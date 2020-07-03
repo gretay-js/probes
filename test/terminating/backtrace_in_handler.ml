ca0598a063f138c71798b384c2832fe7ff24c0a6
Greta Yorsh <gyorsh@janestreet.com>
2020-07-03 13:03:30 +0100

    make tests pass on both flambda and non-flambda builds

--- test/terminating/backtrace_in_handler.ml
+++ test/terminating/backtrace_in_handler.ml
@@ -1,9 +1,10 @@
-let bt s =
-  Printf.printf "%s:\n" s;
-  Printexc.print_raw_backtrace stdout (Printexc.get_callstack 9999)
+let bt title =
+  Printexc.get_callstack 9999
+  |> Printexc.raw_backtrace_to_string
+  |> Str.global_replace (Str.regexp_string " (inlined)") ""
+  |> Printf.printf "%s:\n%s" title
 
 let () =
   bt "Before";
   [%probe "backtrace" (bt "Inside")];
   bt "After"
-
