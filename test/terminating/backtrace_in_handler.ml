e4d00ffce89dd746e52750cdf89dfee8bfa84e8c
Greta Yorsh <gyorsh@janestreet.com>
2020-07-31 17:41:16 +0100

    fix for non-optimized build

--- test/terminating/backtrace_in_handler.ml
+++ test/terminating/backtrace_in_handler.ml
@@ -3,7 +3,7 @@ let bt title =
      such as flambda, so we don't print the backtrace. *)
   let len = Printexc.(get_callstack 9999
                       |> raw_backtrace_length) in
-  Printf.printf "%s is backtrace length > 2? %b\n" title (len > 2)
+  Printf.printf "%s is backtrace length >= 2? %b\n" title (len >= 2)
 
 let () =
   bt "Before";
